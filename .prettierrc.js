94851b60a17ee414330530ee316a6490642f8725
Rinat27072021 <tul-r@yandex.ru>
2022-11-15 10:06:56 +0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: false, // замените это свойство на противоположное
+  trailingComma: "all",
+  semi: true,
+  jsxBracketSameLine: true,
+  printWidth: 100,
+};
